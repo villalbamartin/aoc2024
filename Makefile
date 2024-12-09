d4f850e345288941cdf7ec2201c5733a52755450
Martin Villalba <villalba@7c0h.com>
2024-12-09 16:36:20 +0100

    Adds a solution for the first part of day 9.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC=gcc
 CFLAGS=`pkg-config --cflags glib-2.0` -Wall -pedantic
 LIBS=`pkg-config --libs glib-2.0` -lm
 
-all: day1 day2 day3 day4 day5 day6
+all: day1 day2 day3 day4 day5 day6 day7 day8 day9
 	echo "Making all"
 
 day1: day1.o
@@ -24,6 +24,12 @@ day6: day6.o utils.o
 	$(CC) $(CFLAGS) -g -o $@ $< utils.o $(LIBS)
 
 day7: day7.o
+	$(CC) $(CFLAGS) -g -o $@ $< $(LIBS)
+
+day8: day8.o utils.o
+	$(CC) $(CFLAGS) -g -o $@ $< utils.o $(LIBS)
+
+day9: day9.o
 	$(CC) $(CFLAGS) -g -o $@ $< $(LIBS)
 
 %.o: %.c
---
+++ day9.c
@@ -0,0 +1,112 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+unsigned long int checksum(int *mem)
+{
+	unsigned long int retval = 0;
+	size_t curr_pos = 0;
+	while (mem[curr_pos] != -1)
+	{
+		retval += curr_pos * mem[curr_pos];
+		curr_pos += 1;
+	}
+	return retval;
+}
+
+void day9_1(char *filename)
+{
+	/**
+	 * Solves the first problem for day 9 of the AoC and prints the answer.
+	 * @param filename path to the file containing the input data.
+	 */
+
+	FILE *fp;  // Pointer to open a file
+	char *line = NULL;  // Line currently being read
+	size_t len = 0;  // Parameter for getline()
+
+	int *mem;  // Representation of the state of memory
+	int curr_id = 0;  // ID of the current file
+	size_t curr_pos =
+	    0;  // Index for the current position when looking forward
+	size_t curr_free =
+	    0;  // Index for the position of the first block of free memory
+	bool is_space =
+	    false;  // Whether we are reading the size of a space or a file
+
+	// Read the number of rows and columns
+	fp = fopen(filename, "r");
+	// Since we read a single line we don't need a "while" loop
+	getline(&line, &len, fp);
+	// We reserve 9 times as much memory as characters - worse case
+	// scenarion is a line full of 9s. We are wasting some memory, but my PC
+	// can take it.
+	mem = (int *)malloc(sizeof(int) * strlen(line) * 9);
+	// Initialize all memory to -1
+	memset(mem, -1, sizeof(int) * strlen(line) * 9);
+
+	for (size_t i = 0; i < strlen(line); i++)
+	{
+		// This was the source of the one bug I had
+		if (line[i] == '\n')
+		{
+			break;
+		}
+		if (!is_space)
+		{
+			// We write as many IDs as necesary and increase the ID
+			// counter
+			for (size_t j = 0; j < line[i] - '0'; j++)
+			{
+				mem[curr_pos + j] = curr_id;
+			}
+			curr_id += 1;
+		}
+		else if (curr_free == 0)
+		{
+			// We keep track of where the first free position is.
+			// This code is only called once.
+			curr_free = curr_pos;
+		}
+		// Increase the index of the current position and switch between
+		// space / file
+		curr_pos += line[i] - '0';
+		is_space = !is_space;
+	}
+
+	// End of the used memory - prevent off-by-one
+	curr_pos -= 1;
+	while (curr_free < curr_pos)
+	{
+		// Found some non-free memory at the end that needs to be moved
+		// to the beginning of the memory. We do that here by decreasing
+		// the curr_pos counter and increasing the curr_free pointer.
+		if (mem[curr_pos] != -1)
+		{
+			mem[curr_free] = mem[curr_pos];
+			mem[curr_pos] = -1;
+			curr_free += 1;
+			while (mem[curr_free] != -1)
+			{
+				curr_free += 1;
+			}
+			while (mem[curr_pos] == -1)
+			{
+				curr_pos -= 1;
+			}
+		}
+	}
+	// If we reached here the pointers have crossed and I am done sorting
+	// the file. All that remains is the checksum.
+	printf("%lu\n", checksum(mem));
+	free(mem);
+	free(line);
+	fclose(fp);
+}
+
+int main(int argc, char **argv)
+{
+	day9_1("inputs/day9.txt");
+	return 0;
+}
