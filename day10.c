c83db63c67be96567a991bcd059afdf8821db906
Martin Villalba <villalba@7c0h.com>
2024-12-17 23:49:55 +0100

    Adds a solution for problem 2 of day 10

--- day10.c
+++ day10.c
@@ -24,7 +24,6 @@ unsigned int new_trails_rec(char **map, size_t row, size_t col, size_t rows, siz
 		size_t high_row = (row >= rows - 1)? row : row + 1;
 		size_t high_col = (col >= cols - 1)? col : col + 1;
 		
-
 		for (size_t i = low_row; i <= high_row; i++)
 		{
 			for (size_t j = low_col; j <= high_col; j++)
@@ -75,6 +74,25 @@ unsigned int new_trails(char **map, size_t row, size_t col, size_t rows, size_t 
 	return num_reachable;	
 }
 
+
+unsigned int new_trails_det(char **map, size_t row, size_t col, size_t rows, size_t cols)
+{
+	char** reachable = (char**) malloc (sizeof(char*) * rows);
+	unsigned int num_reachable = 0;
+	for (size_t i=0; i<rows; i++)
+	{
+		reachable[i] = (char*) malloc(sizeof(char) * cols);
+		memset(reachable[i],'\0', sizeof(char) * cols);
+	}
+	num_reachable += new_trails_rec(map, row, col, rows, cols, '0', reachable);
+	for (size_t i=0; i<rows; i++)
+	{
+		free(reachable[i]);
+	}
+	free(reachable);
+	return num_reachable;	
+}
+
 void day10_1(char *filename)
 {
 	/**
@@ -109,9 +127,43 @@ void day10_1(char *filename)
 }
 
 
+void day10_2(char *filename)
+{
+	/**
+	 * Solves the second problem for day 10 of the AoC and prints the answer.
+	 * @param filename path to the file containing the input data.
+	 */
+	char **map;   // Representation of the map
+	size_t rows;  // Number of rows in the map
+	size_t cols;  // Number of columns in the map
+	unsigned int num_trails = 0;
+
+	map = get_square_input(filename, &rows, &cols);
+	for (size_t row=0; row < rows; row++)
+	{
+		for (size_t col=0; col < cols; col++)
+		{
+			if (map[row][col] == '0')
+			{
+				num_trails += new_trails_det(map, row, col, rows, cols);
+			}
+		}
+	}
+	// Print the solution
+	printf("%u\n", num_trails);
+
+	// Free memory
+	for (size_t i = 0; i < rows; i++)
+	{
+		free(map[i]);
+	}
+	free(map);
+}
+
+
 int main(int argc, char **argv)
 {
 	day10_1("inputs/day10.txt"); // 607
-	//day10_2("inputs/day10.txt");
+	day10_2("inputs/day10.txt"); // 1384
 	return 0;
 }
